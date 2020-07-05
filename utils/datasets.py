9c55360bd22058d78dda3b0ca58c6ad7853d7a94
NanoCode012 <kevinvong@rocketmail.com>
2020-07-05 19:06:39 +0700

    Test increase number of workers

--- utils/datasets.py
+++ utils/datasets.py
@@ -54,7 +54,7 @@ def create_dataloader(path, imgsz, batch_size, stride, opt, hyp=None, augment=Fa
     batch_size = min(batch_size, len(dataset))
     if (hasattr(opt, "world_size")):
         #nw=int((nw+opt.world_size-1)/opt.world_size)  # https://github.com/pytorch/examples/blob/19ae7a30593ab31704d54493f49acc32f316da79/imagenet/main.py#L152
-        nw = min([os.cpu_count()//opt.world_size, batch_size if batch_size > 1 else 0, 8])
+        nw = min([os.cpu_count()//opt.world_size, batch_size if batch_size > 1 else 0])
     else:
         nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])
 
